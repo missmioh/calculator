f72b5d9dc384eb733fdad71ddec32f8a9a5a7a40
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-25 18:16:45 +0100

    Add event listener for digit buttons

--- script.js
+++ script.js
@@ -7,21 +7,31 @@ function operate(operator, a, b) {
     if (operator === "-") return subtraction(a, b);
     if (operator === "*") return multiplication(a, b);
     if (operator === "/") return division(a, b);
-}
+};
 
 
 function addition(a, b) {
     return a + b;
-}
+};
 
 function subtraction(a, b) {
     return a - b;
-}
+};
 
 function multiplication(a, b) {
     return a * b;
-}
+};
 
 function division(a, b) {
     return a / b;
-}
+};
+
+const digitBtns = document.querySelectorAll(".digitBtn");
+const display = document.querySelector("p");
+
+digitBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        firstOperand = e.target.id;
+        display.textContent = `${e.target.id}`;
+    });
+});
