ca71d622a9a27ad4d6c9b102cbc20085758376fb
missmioh <186388075+missmioh@users.noreply.github.com>
2026-02-25 18:35:03 +0100

    Add event listeners for operator buttons

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let firstOperand;
 let secondOperand;
 let operator;
 
+// the math
+
 function operate(operator, a, b) {
     if (operator === "+") return addition(a, b);
     if (operator === "-") return subtraction(a, b);
@@ -26,12 +28,29 @@ function division(a, b) {
     return a / b;
 };
 
+// click events
+
 const digitBtns = document.querySelectorAll(".digitBtn");
 const display = document.querySelector("p");
 
 digitBtns.forEach((button) => {
     button.addEventListener("click", (e) => {
-        firstOperand = e.target.id;
-        display.textContent = `${e.target.id}`;
+        firstOperand = parseInt(e.target.id);
+        display.textContent = e.target.id;
+    });
+});
+
+const operatorBtns = document.querySelectorAll(".operatorBtn");
+
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.id !== "=") {
+            operator = e.target.id;
+            display.textContent = firstOperand + e.target.id;
+        };
+
+        if (e.target.id === "=") {
+            display.textContent = operate(operator, firstOperand, 2);
+        };
     });
 });
